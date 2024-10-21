413429bbb371fe0cf5d15ef9af65552b2352bb04
Cabbarow <167204721+Cabbarow@users.noreply.github.com>
2024-10-21 17:26:17 +0300

    vite config set

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // Projende alias kullanarak yolları kısaltabilirsin
+  build: {
+    outDir: 'dist',       
+    sourcemap: false,      
+    rollupOptions: {
+      input: '/index.html', 
     },
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom','react-icons','@mui/material', '@emotion/react', '@emotion/styled'],
-  },
-})
+});
